89df1405baf6d72ab6bbf3a4a465e6a3b1c7094d
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-11-18 23:00:42 +0600

    UPDATE: currently working on making the navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,26 +5,7 @@ class Navigationbar extends React.Component {
         render() {
                 return (
                         <Navbar collapseOnSelect expand='lg'>
-                                <Navbar.Brand href='/' className='companyName'>
-
-                                </Navbar.Brand>
-                                <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-                                <Navbar.Collapse id='responsive-navbar-nav'>
-                                        <Nav className='mr-auto'>
-                                                <Nav.Link href='/about_and_contact'>Contact Us</Nav.Link>
-                                        </Nav>
-                                        <Nav>
-                                                <Nav.Link href='donate'>Donate</Nav.Link>
-                                        </Nav>
-                                        <Form inline>
-                                                <Button variant='link' className='login' onClick={this.handleShowLogin}>
-                                                        Login
-                                </Button>
-                                                <Button className='button buttonText' onClick={this.handleShow}>
-                                                        <span>Sign Up</span>
-                                                </Button>
-                                        </Form>
-                                </Navbar.Collapse>
+                                nav
                         </Navbar>
                 );
         }
