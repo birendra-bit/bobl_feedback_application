71607206a25ccc35950ae0eee5be9c4d1e25b035
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-11-19 19:56:05 +0600

    CREATED: login jumbotron

--- src/containers/login/login.js
+++ src/containers/login/login.js
@@ -1,17 +1,14 @@
 import React from "react";
-import NavigationBar from "../../components/navbar/navbar";
-import { Button, Container, Col, Row, Form } from "react-bootstrap"
+import LoginJumbotron from "../../components/login/login";
+import { Row, Col, Container } from "react-bootstrap"
 import "./login.css"
 
 function Login() {
         return (
-                <Container fluid={true}>
+                <Container fluid={true} className="loginBackground">
                         <Row>
-                                <Col md={12} lg={12} sm={12} xs={12} style={{ border: "1px solid red" }}>
-                                        {/* <NavigationBar /> */}
-                                </Col>
-                                <Col md={12} lg={12} sm={12} xs={12} className={"loginBackground"} style={{ border: "1px solid green" }}>
-                                        <h1> djhfkjshfkjdshfkjsd </h1>
+                                <Col md={4} lg={4} sm={4} xs={4}>
+                                        <LoginJumbotron />
                                 </Col>
                         </Row>
                 </Container>
@@ -19,4 +16,5 @@ function Login() {
 }
 
 export default (Login);
+
 
