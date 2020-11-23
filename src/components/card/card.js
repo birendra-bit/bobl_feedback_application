06610685dd1e4a0034c07a4735cdf0c306c5f54c
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-11-23 09:53:42 +0600

    UPDATE:working on login

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -4,15 +4,15 @@ import UserImage from '../../assets/images/user-male-icon.png'
 import './card.css'
 
 
-const Cards =props =>(
+const Cards = props => (
     <Card sm={12} md={12} lg={4} className="m-1 text-center pt-1 card" >
-        <Card.Img className="m-auto" style={{ width: '100px',height:'100px'}} src={UserImage}></Card.Img>
+        <Card.Img className="m-auto" style={{ width: '100px', height: '100px' }} src={UserImage}></Card.Img>
         <Card.Body >
-            <Card.Title style={{fontSize:'12px'}}>Name : Tshering Dorji</Card.Title>
-            <Card.Text style={{fontSize:'10px'}}>
-                Designation: Chief<br/>
-                Department: HR <br/>
-                Contact Number: 17892536<br/>
+            <Card.Title style={{ fontSize: '12px' }}>Name : Tshering Dorji</Card.Title>
+            <Card.Text style={{ fontSize: '10px' }}>
+                Designation: Chief<br />
+                Department: HR <br />
+                Contact Number: 17892536<br />
                 Email: example@bobl.bt
             </Card.Text>
             <Button className="btn-primary btn-sm btn-block">Give Feeds</Button>
