aee7ad73dcccc8a1e120f27d6fb80b1ace8748a8
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-11-24 17:13:51 +0600

    PROGRESS: working on displayng the details

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,29 +1,31 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom'
-import Dashboard from './containers/dashboard/dashboard'
-import Login from './containers/login/login'
-
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Dashboard from "./containers/dashboard/dashboard";
+import Login from "./containers/login/login";
+import GiveFeedback from "./containers/give_feedback/give_feedback";
 
 class AppRouter extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            isLoggedIn: false
-        }
+            isLoggedIn: false,
+        };
     }
     render() {
         return (
             <Switch>
-                <Route path="/" exact>
+                <Route path='/' exact>
                     <Dashboard />
                 </Route>
-                <Route path="/login">
+                <Route path='/login'>
                     <Login />
                 </Route>
+                <Route path='/give_feedback'>
+                    <GiveFeedback />
+                </Route>
             </Switch>
-        )
+        );
     }
 }
 
-
-export default AppRouter
+export default AppRouter;
