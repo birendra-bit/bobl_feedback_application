29f8e1f23183197069b6e2b9c676146db9c0dda8
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-11-24 12:24:41 +0600

    FIX: made the footer stick to the bottom, changed the text color for the cards

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -4,29 +4,22 @@ import UserImage from "../../assets/images/user-male-icon.png";
 import "./card.css";
 
 const Cards = (props) => (
-  <Card sm={12} md={12} lg={4} className="m-1 text-center pt-1 card">
-    <Card.Img
-      className="m-auto"
-      style={{ width: "70px", height: "70px" }}
-      src={UserImage}
-    ></Card.Img>
-    <Card.Body>
-      <Card.Title style={{ fontSize: "11px" }}>
-        {props.info.name}{" "}
-      </Card.Title>
-      <Card.Text style={{ fontSize: "10px" }}>
-        Designation:{props.info.designation} <br />
-        Department: {props.info.department} <br />
-        Email: {props.info.email}
-      </Card.Text>
-      <Button
-        className="btn-primary btn-block"
-        style={{ padding: "3px", fontSize: "11px", borderRadius: "40px" }}
-      >
-        {props.btnText}
-      </Button>
-    </Card.Body>
-  </Card>
+    <Card sm={12} md={12} lg={4} className='m-1 text-center pt-1 card'>
+        <Card.Img className='m-auto' style={{ width: "70px", height: "70px" }} src={UserImage}></Card.Img>
+        <Card.Body>
+            <Card.Title style={{ fontSize: "11px", color: "#2050ab" }}>{props.info.name}</Card.Title>
+            <Card.Text style={{ fontSize: "10px" }}>
+                <span style={{ color: "gray" }}> Designation: </span>
+                {props.info.designation} <br />
+                <span style={{ color: "gray" }}>Department:</span> {props.info.department} <br />
+                <span style={{ color: "gray" }}>Email: </span>
+                {props.info.email}
+            </Card.Text>
+            <Button className='btn-primary btn-block' style={{ padding: "3px", fontSize: "11px", borderRadius: "40px" }}>
+                {props.btnText}
+            </Button>
+        </Card.Body>
+    </Card>
 );
 
 export default Cards;
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -44,10 +44,10 @@ function Navigationbar() {
             <Modal show={show} onHide={handleClose} animation={true}>
                 <Modal.Body style={{ padding: "20px" }}>
                     <Row>
-                        <Col md={5}>
+                        <Col md={5} lg={5} sm={5} xs={5}>
                             <img src={UserImage} alt='user' className='user_image' />
                         </Col>
-                        <Col md={7} style={{ marginTop: "20px" }}>
+                        <Col md={7} lg={7} sm={7} xs={7} style={{ marginTop: "20px" }}>
                             <p>
                                 <span className='info'> Name: </span> Phuntsho Norbu{" "}
                             </p>
