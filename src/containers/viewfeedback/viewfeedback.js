b9836382237c2fd84b527cb5fe0654e65506e70f
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-11-26 14:13:06 +0600

    package json update

--- src/containers/viewfeedback/viewfeedback.js
+++ src/containers/viewfeedback/viewfeedback.js
@@ -15,69 +15,70 @@ class ViewFeedBack extends Component {
         this.state = {
             show: false,
             user_completed: true,
-            data: [
-                {
-                    competency_code: "CD3: Delivery003",
-                    competency: [
-                        {
-                            title: "To ensure closures and deliver on intended results prudently.",
-                            detail: [
-                                { content: "Always conscious about resources against results.", score: "", id: "q11a" },
-                                { content: "Always conscious about resources against results.", score: "", id: "q11b" },
-                                { content: "Always conscious about resources against results.", score: "", id: "q11c" },
-                                { content: "Always conscious about resources against results.", score: "", id: "q11d" },
-                            ],
-                        },
-                        {
-                            title: "To steer and implement changes, and deals with uncertainties.",
-                            detail: [
-                                { content: "Always conscious about resources against results.", score: "", id: "q12a" },
-                                { content: "Always conscious about resources against results.", score: "", id: "q12b" },
-                            ],
-                        },
-                        {
-                            title: "To build organizational capacity and harness individual potentials",
-                            detail: [
-                                { content: "Always conscious about resources against results.", score: "", id: "q13a" },
-                                { content: "Always conscious about resources against results.", score: "", id: "q13b" },
-                                { content: "Always conscious about resources against results.", score: "", id: "q13c" },
-                            ],
-                        },
-                    ],
-                },
-                {
-                    competency_code: "CD2: Drive004",
-                    competency: [
-                        {
-                            title: "To communicate effectively.",
-                            detail: [
-                                { content: "Always conscious about resources against results.", score: "", id: "q21a" },
-                                { content: "Always conscious about resources against results.", score: "", id: "q21b" },
-                            ],
-                        },
-                        {
-                            title: "To exemplify personal drive and integrity.",
-                            detail: [
-                                { content: "Always conscious about resources against results.", score: "", id: "q22a" },
-                                { content: "Always conscious about resources against results.", score: "", id: "q22b" },
-                            ],
-                        },
-                        {
-                            title: "To cultivate positive and conducive work culture",
-                            detail: [{ content: "Always conscious about resources against results.", score: "", id: "q23a" }],
-                        },
-                    ],
-                },
-            ],
+            data: [],
+            // data: [
+            //     {
+            //         competency_code: "CD3: Delivery003",
+            //         competency: [
+            //             {
+            //                 title: "To ensure closures and deliver on intended results prudently.",
+            //                 detail: [
+            //                     { content: "Always conscious about resources against results.", score: "", id: "q11a" },
+            //                     { content: "Always conscious about resources against results.", score: "", id: "q11b" },
+            //                     { content: "Always conscious about resources against results.", score: "", id: "q11c" },
+            //                     { content: "Always conscious about resources against results.", score: "", id: "q11d" },
+            //                 ],
+            //             },
+            //             {
+            //                 title: "To steer and implement changes, and deals with uncertainties.",
+            //                 detail: [
+            //                     { content: "Always conscious about resources against results.", score: "", id: "q12a" },
+            //                     { content: "Always conscious about resources against results.", score: "", id: "q12b" },
+            //                 ],
+            //             },
+            //             {
+            //                 title: "To build organizational capacity and harness individual potentials",
+            //                 detail: [
+            //                     { content: "Always conscious about resources against results.", score: "", id: "q13a" },
+            //                     { content: "Always conscious about resources against results.", score: "", id: "q13b" },
+            //                     { content: "Always conscious about resources against results.", score: "", id: "q13c" },
+            //                 ],
+            //             },
+            //         ],
+            //     },
+            //     {
+            //         competency_code: "CD2: Drive004",
+            //         competency: [
+            //             {
+            //                 title: "To communicate effectively.",
+            //                 detail: [
+            //                     { content: "Always conscious about resources against results.", score: "", id: "q21a" },
+            //                     { content: "Always conscious about resources against results.", score: "", id: "q21b" },
+            //                 ],
+            //             },
+            //             {
+            //                 title: "To exemplify personal drive and integrity.",
+            //                 detail: [
+            //                     { content: "Always conscious about resources against results.", score: "", id: "q22a" },
+            //                     { content: "Always conscious about resources against results.", score: "", id: "q22b" },
+            //                 ],
+            //             },
+            //             {
+            //                 title: "To cultivate positive and conducive work culture",
+            //                 detail: [{ content: "Always conscious about resources against results.", score: "", id: "q23a" }],
+            //             },
+            //         ],
+            //     },
+            // ],
         };
     }
 
     componentDidMount() {
-        // axios.get("/api/method/erpnext.feedback_api.get_feedback_employee?user=" + sessionStorage.getItem("user")).then((res) => {
-        //     const persons = res.data;
-        //     console.log(persons);
-        // });
-       
+        axios.get("/api/method/erpnext.feedback_api.get_competency").then((res) => {
+            this.setState({ data: res.data.message });
+            const persons = res.data.message;
+            console.log(persons);
+        });
     }
 
     // updates the score on the state
@@ -110,7 +111,7 @@ class ViewFeedBack extends Component {
             console.log("hello");
         } else {
             alert("please complete all the answer");
-            $(".show_alert").css("display", "none");
+            // $(".show_alert").css("display", "none");
         }
     };
 
@@ -147,7 +148,7 @@ class ViewFeedBack extends Component {
                                     <React.Fragment key={index}>
                                         <Row>
                                             <Col lg={2}>
-                                                <strong>{x.competency_code}</strong>
+                                                <strong>{x.corporate_ds}</strong>
                                             </Col>
                                             <Col lg={10}>
                                                 {x.competency.map((c, i) => {
