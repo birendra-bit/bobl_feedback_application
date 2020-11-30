3b0e73d63499c443debc46a20cecf35713e0fa8b
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-11-30 10:08:29 +0600

    CHANGE: changed the logo image and the footer text color

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Navbar, Button, Image, Modal, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./navbar.css";
-import Logo from "../../assets/images/BOBLogo.png";
+import Logo from "../../assets/images/navbar-logo.png";
 import UserImage from "../../assets/images/user-male-icon.png";
 import { Link } from "react-router-dom";
 
@@ -19,8 +19,7 @@ function Navigationbar() {
     return (
         <React.Fragment>
             <Navbar collapseOnSelect expand='lg' className='nav'>
-                &nbsp; &nbsp;
-                <Navbar.Brand className='mr-auto'>
+                <Navbar.Brand className='navbar-logo-container'>
                     <Link to='/dashboard'>
                         <Image className='logo' src={Logo} />
                     </Link>
