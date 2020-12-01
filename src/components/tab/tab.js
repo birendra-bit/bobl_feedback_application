9e45326b128c8ec547389428545af61fc3bc24cd
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-12-01 14:58:35 +0600

    FIX: changed the looks for the tab header

--- src/components/tab/tab.js
+++ src/components/tab/tab.js
@@ -1,28 +1,25 @@
 import React from "react";
 import "./tab.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowAltCircleDown, faPen } from "@fortawesome/free-solid-svg-icons";
 
 const tab = (props) => {
-  return (
-    <div>
-      <div className="d-flex tab">
-        <span
-          className={props.giveFeeds ? "active" : null}
-          onClick={props.giveFeedsToggleHandler}
-        >
-          Give Feeds &#10146;
-        </span>
-        &nbsp;&nbsp; | &nbsp;&nbsp;
-        <span
-          className={props.giveFeeds ? null : "active"}
-          onClick={props.feedGivenToggle}
-        >
-          Feeds Given By &#10146;
-        </span>
-      </div>
-      <hr />
-    </div>
-  );
+    return (
+        <div>
+            <div className='d-flex tab'>
+                <span className={props.giveFeeds ? "active" : null} onClick={props.giveFeedsToggleHandler} style={{ background: "#8080805c" }} id='give-span'>
+                    Give Feeds &nbsp;
+                    <FontAwesomeIcon icon={faPen} />
+                </span>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <span className={props.giveFeeds ? null : "active"} onClick={props.feedGivenToggle} style={{ background: "#8080805c" }} id='view-span'>
+                    Feeds Given By &nbsp;
+                    <FontAwesomeIcon icon={faArrowAltCircleDown} />
+                </span>
+            </div>
+            <hr id='tab-hr' />
+        </div>
+    );
 };
 
 export default tab;
