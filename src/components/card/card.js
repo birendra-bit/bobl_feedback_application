c5d426df81f1516d567781aec4922a6a1f5fee02
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-12-02 11:36:54 +0600

    FEAT: give/view button animation upon card hovering

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -13,7 +13,7 @@ const Cards = (props) => {
     const { info } = props;
     if (!info) return <strong>No Data to Display</strong>;
     return (
-        <Card sm={12} md={12} lg={4} className='m-1 text-center pt-1 card'>
+        <Card sm={12} md={12} lg={4} className='m-1 text-center pt-1 user-card'>
             <Card.Img
                 style={{
                     width: "90px",
@@ -21,31 +21,33 @@ const Cards = (props) => {
                     borderRadius: "50%",
                     margin: "15px auto 0px auto",
                 }}
+                className='user-photo-on-card'
                 src={info.image ? "http://192.168.70.38" + info.image : info.gender === "Male" ? imgUrlMale : imgUrlFemale}
             />
             <Card.Body>
                 <Card.Title style={{ fontSize: "15px" }}>{info.employee_name} </Card.Title>
                 <Card.Text style={{ fontSize: "12px" }}>
                     <img src={Red} className='color-bio-info' alt='designation' />
-                    {info.designation} <br />
+                    <span className='card-bio-label'>{info.designation} </span>
+                    <br />
                     <img src={Green} className='color-bio-info' alt='' />
-                    {info.department}
+                    <span className='card-bio-label'>{info.department}</span>
                     <br />
                     <img src={Blue} className='color-bio-info' alt='red-color' />
-                    {info.branch}
+                    <span className='card-bio-label'>{info.branch}</span>
                     <br />
                     <img src={Brown} className='color-bio-info' alt='red-color' />
-                    {info.email}
+                    <span className='card-bio-label'>{info.email}</span>
                 </Card.Text>
                 {info.status === "Pending" ? (
                     <Link to={`/feedback/${info.employee}`}>
-                        <Button className='btn-primary btn-block' style={{ padding: "3px", fontSize: "11px", borderRadius: "40px" }}>
+                        <Button className='btn-primary btn-block view-give-feed-btn' style={{ padding: "3px", fontSize: "11px", borderRadius: "40px" }}>
                             Give Feeds
                         </Button>
                     </Link>
                 ) : (
                     // <Link to={`/view/${info.employee}`}>
-                    <Button disabled={true} className='btn-success btn-block' style={{ padding: "3px", fontSize: "11px", borderRadius: "40px" }}>
+                    <Button disabled={true} className='btn-success btn-block view-give-feed-btn' style={{ padding: "3px", fontSize: "11px", borderRadius: "40px" }}>
                         View Feeds
                     </Button>
                     // </Link>
--- src/containers/dashboard/dashboard.js
+++ src/containers/dashboard/dashboard.js
@@ -14,144 +14,140 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
 
 class Dashboard extends Component {
-  constructor(props = []) {
-    super(props);
-    this.state = {
-      giveFeedsData: [],
-      reciveFeedsData: [],
-      giveFeeds: true,
-      feedsGiven: false,
-      loader: true,
+    constructor(props = []) {
+        super(props);
+        this.state = {
+            giveFeedsData: [],
+            reciveFeedsData: [],
+            giveFeeds: true,
+            feedsGiven: false,
+            loader: true,
+        };
+    }
+
+    //handle toggle
+    giveFeedsToggleHandler = () => {
+        this.setState({
+            giveFeeds: true,
+        });
     };
-  }
-
-  //handle toggle
-  giveFeedsToggleHandler = () => {
-    this.setState({
-      giveFeeds: true,
-    });
-  };
-  feedGivenToggle = () => {
-    this.setState({
-      giveFeeds: false,
-    });
-  };
-
-  //get give feedback data
-  getGiveFeedsUserData = async () => {
-    let url = `/api/method/erpnext.feedback_api.get_feedback_provide?user=`;
-    try {
-      let resp = await axios.get(url + sessionStorage.getItem("user"));
-      if (resp.data.message !== "No Data")
+    feedGivenToggle = () => {
         this.setState({
-          giveFeedsData: resp.data.message,
+            giveFeeds: false,
         });
-        this.loaderHandler()
-    } catch (err) {
-      console.error(err);
-    }
-  };
-loaderHandler = ()=>{
-  this.setState({
-    loader: false
-  })
-}
-  //get Receive feeds data
-  getReciveFeedsUserData = async () => {
-    let url = `/api/method/erpnext.feedback_api.get_feedback_receive?user=`;
-    try {
-      let resp = await axios.get(url + sessionStorage.getItem("user"));
-      if (resp.data.message !== "No Data")
+    };
+
+    //get give feedback data
+    getGiveFeedsUserData = async () => {
+        let url = `/api/method/erpnext.feedback_api.get_feedback_provide?user=`;
+        try {
+            let resp = await axios.get(url + sessionStorage.getItem("user"));
+            if (resp.data.message !== "No Data")
+                this.setState({
+                    giveFeedsData: resp.data.message,
+                });
+            this.loaderHandler();
+        } catch (err) {
+            console.error(err);
+        }
+    };
+    loaderHandler = () => {
         this.setState({
-          reciveFeedsData: resp.data.message,
+            loader: false,
         });
-    } catch (err) {
-      console.error(err);
+    };
+    //get Receive feeds data
+    getReciveFeedsUserData = async () => {
+        let url = `/api/method/erpnext.feedback_api.get_feedback_receive?user=`;
+        try {
+            let resp = await axios.get(url + sessionStorage.getItem("user"));
+            if (resp.data.message !== "No Data")
+                this.setState({
+                    reciveFeedsData: resp.data.message,
+                });
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    componentDidMount() {
+        this.getGiveFeedsUserData();
+        this.getReciveFeedsUserData();
+        // setTimeout(this.loaderHandler,2000)
     }
-  };
-
-  componentDidMount() {
-    this.getGiveFeedsUserData();
-    this.getReciveFeedsUserData();
-    // setTimeout(this.loaderHandler,2000)
-  }
-  render() {
-    let cards = this.state.giveFeedsData.map((info, index) => {
-      return (
-        <Col sm={4} md={4} lg={3} key={index}>
-          <Card info={info} btnText="Give Feeds" />
-        </Col>
-      );
-    });
-    let label = (
-      <React.Fragment>
-        <span style={{ color: "black", fontSize: "15px", margin: "0 20px" }}>
-          <img src={Red} alt="designation" style={{ width: "10px" }} />
-          &nbsp;&nbsp;Designation
-        </span>
-        <span style={{ color: "black", fontSize: "15px", margin: "0 20px" }}>
-          &nbsp;
-          <img src={Green} alt="designation" style={{ width: "10px" }} />
-          &nbsp;&nbsp;Department
-        </span>
-        <span style={{ color: "black", fontSize: "15px", margin: "0 20px" }}>
-          &nbsp;
-          <img src={Blue} alt="designation" style={{ width: "10px" }} />
-          &nbsp;&nbsp;Branch
-        </span>
-        <span style={{ color: "black", fontSize: "15px", margin: "0 20px" }}>
-          &nbsp;
-          <img src={Brown} alt="designation" style={{ width: "10px" }} />
-          &nbsp;&nbsp;Email
-        </span>
-      </React.Fragment>
-    );
-    let feedsGivenList = <FeedbackGivenBy info={this.state.reciveFeedsData} />;
-    return (
-      <React.Fragment>
-        <Navigationbar />
-        <Container fluid={false}>
-          <br />
-          <Tab
-            giveFeeds={this.state.giveFeeds}
-            giveFeedsToggleHandler={this.giveFeedsToggleHandler}
-            feedGivenToggle={this.feedGivenToggle}
-          />
-          <Row>
-            {this.state.loader ? (
-              <Col>
-                <div className="loader">
-                  <Loader type="Circles" color="#00BFFF" height={80} width={80} />
-                </div>
-              </Col>
-            ) : (
-              <React.Fragment>
-                <Col sm={12} md={12} lg={12} xs={12}>
-                  <Row>{this.state.giveFeeds ? cards : feedsGivenList}</Row>
-                  <Row>
-                    <Col md={12} xs={12} lg={12}>
-                      <br />
-                      <br />
-                      <hr />
-                    </Col>
-
-                    <Col md={12} xs={12} lg={12}>
-                      {this.state.giveFeeds ? label : ""}
-                    </Col>
-                    <Col md={12} xs={12} lg={12}>
-                      <hr />
-                    </Col>
-                  </Row>
+    render() {
+        let cards = this.state.giveFeedsData.map((info, index) => {
+            return (
+                <Col sm={4} md={4} lg={3} key={index}>
+                    <Card info={info} btnText='Give Feeds' />
                 </Col>
-                <Col sm={12} md={12} lg={4}></Col>
-              </React.Fragment>
-            )}
-          </Row>
-        </Container>
-        <Footer />
-      </React.Fragment>
-    );
-  }
+            );
+        });
+        let label = (
+            <React.Fragment>
+                <span style={{ color: "black", fontSize: "15px", margin: "0 20px" }}>
+                    <img src={Red} alt='designation' style={{ width: "10px" }} />
+                    &nbsp;&nbsp;Designation
+                </span>
+                <span style={{ color: "black", fontSize: "15px", margin: "0 20px" }}>
+                    &nbsp;
+                    <img src={Green} alt='designation' style={{ width: "10px" }} />
+                    &nbsp;&nbsp;Department
+                </span>
+                <span style={{ color: "black", fontSize: "15px", margin: "0 20px" }}>
+                    &nbsp;
+                    <img src={Blue} alt='designation' style={{ width: "10px" }} />
+                    &nbsp;&nbsp;Branch
+                </span>
+                <span style={{ color: "black", fontSize: "15px", margin: "0 20px" }}>
+                    &nbsp;
+                    <img src={Brown} alt='designation' style={{ width: "10px" }} />
+                    &nbsp;&nbsp;Email
+                </span>
+            </React.Fragment>
+        );
+        let feedsGivenList = <FeedbackGivenBy info={this.state.reciveFeedsData} />;
+        return (
+            <React.Fragment>
+                <Navigationbar />
+                <Container fluid={false}>
+                    <br />
+                    <Tab giveFeeds={this.state.giveFeeds} giveFeedsToggleHandler={this.giveFeedsToggleHandler} feedGivenToggle={this.feedGivenToggle} />
+                    <Row>
+                        {this.state.loader ? (
+                            <Col>
+                                <div className='loader'>
+                                    <Loader type='Circles' color='#00BFFF' height={80} width={80} />
+                                </div>
+                            </Col>
+                        ) : (
+                            <React.Fragment>
+                                <Col sm={12} md={12} lg={12} xs={12}>
+                                    <Row>{this.state.giveFeeds ? cards : feedsGivenList}</Row>
+                                    <Row>
+                                        <Col md={12} xs={12} lg={12}>
+                                            <br />
+                                            <br />
+                                            <hr />
+                                        </Col>
+
+                                        <Col md={12} xs={12} lg={12}>
+                                            {this.state.giveFeeds ? label : ""}
+                                        </Col>
+                                        <Col md={12} xs={12} lg={12}>
+                                            <hr />
+                                        </Col>
+                                    </Row>
+                                </Col>
+                                <Col sm={12} md={12} lg={4}></Col>
+                            </React.Fragment>
+                        )}
+                    </Row>
+                </Container>
+                <Footer />
+            </React.Fragment>
+        );
+    }
 }
 
 export default Dashboard;
