5d641e6fd236b9c228037e66b5b2bc739f34fa85
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-12-10 17:10:47 +0600

    ADDing cors package

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,8 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-
+var cors = require("cors");
 module.exports = function (app) {
     app.use(
+        cors(),
         "/api",
         createProxyMiddleware({
             target: "http://192.168.70.38",
