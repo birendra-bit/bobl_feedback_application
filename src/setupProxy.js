6752d0388e6580e194a53980d53cc335a87596d7
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-12-10 17:40:23 +0600

    adding proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,12 @@
-// const { createProxyMiddleware } = require("http-proxy-middleware");
-// var cors = require("cors");
-// module.exports = function (app) {
-//     app.use(
-//         cors(),
-//         "/api",
-//         createProxyMiddleware({
-//             target: "http://192.168.70.38",
-//             changeOrigin: true,
-//         })
-//     );
-//     console.log("inside the setup proxy");
-// };
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+    app.use(
+        "/api",
+        createProxyMiddleware({
+            target: "http://192.168.70.38",
+            changeOrigin: true,
+        })
+    );
+    console.log("inside the setup proxy");
+};
