6b1034272a5a7db821e429443810fed0097b7e0e
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-12-10 17:44:44 +0600

    adding proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,6 +6,11 @@ module.exports = function (app) {
         createProxyMiddleware({
             target: "http://192.168.70.38",
             changeOrigin: true,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": "*",
+                "Access-Control-Allow-Methods": "*",
+            },
         })
     );
     console.log("inside the setup proxy");
